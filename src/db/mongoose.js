53ee9834d62baf09e6ffc61c877d5f62058a6132
Prateek Niket <prateekniket25@gmail.com>
2022-02-07 09:52:48 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-// const connectionURL = 'mongodb+srv://Prateek:node123@cluster0.nwkre.mongodb.net/task-manager-api?retryWrites=true&w=majority'
-
 mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true
